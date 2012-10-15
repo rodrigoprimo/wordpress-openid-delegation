88cab2f67da22de640b878a6d802aa0fd6d724b3
rodrigosprimo <rodrigosprimo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-15 14:08:23 +0000

    initial commit
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-openid-delegation/trunk@612737 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ openid-delegation.php
@@ -0,0 +1,94 @@
+<?php
+
+/*  Copyright 2012 Rodrigo Primo  (email: rodrigo@hacklab.com.br)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Plugin Name: OpenID Delegation
+Plugin URI: https://github.com/rodrigosprimo/wordpress-openid-delegation
+Description: Delegates your blog URL to the OpenID provider you choose. This plugin was built based on MyOpenID Delegation plugin (https://github.com/benatkin/wordpress-myopenid-delegation).
+Version: 0.1
+Author: Rodrigo Primo
+Author URI: http://hacklab.com.br/
+License: GPL2
+*/
+
+//TODO: internationalize the plugin
+
+class OpenIdDelegationPlugin
+{
+    public function init()
+    {
+        add_settings_section(
+            'openid_delegation_section',
+            'OpenID Delegation',
+            array($this, 'settingsDescription'),
+            'general'
+        );
+
+        add_settings_field(
+            'openid_delegation_provider',
+            'OpenID Provider URL',
+            array($this, 'providerField'),
+            'general',
+            'openid_delegation_section'
+        );
+        
+        add_settings_field(
+            'openid_delegate_url',
+            'OpenID Delegate URL',
+            array($this, 'delegateField'),
+            'general',
+            'openid_delegation_section'
+        );
+    
+        register_setting('general', 'openid_delegation_section');
+    }
+    
+    public function settingsDescription()
+    {
+        echo '<p>Please enter your OpenID provider URL and your OpenID delegate URL.</p>';
+    }
+    
+    public function providerField()
+    {
+        echo '<input name="openid_delegation_provider" id="openid_delegation_provider" type="text" value="' . esc_attr(get_option('openid_delegation_provider')) . '" size=20 maxlength=40 /> Example: http://www.myopenid.com/server';
+    }
+
+    public function delegateField()
+    {
+        echo '<input name="openid_delegation_delegate" id="openid_delegation_delegate" type="text" value="' . esc_attr(get_option('openid_delegation_delegate')) . '" size=20 maxlength=40 /> Example: http://YOURUSERNAME.myopenid.com';
+    }
+    
+    public function renderMetaTags()
+    {
+        if (is_home()) {
+            $provider = get_option('openid_delegation_provider');
+            $delegate = get_option('openid_delegation_delegate');
+            if ($provider && $delegate) {
+                echo "<link rel='openid.server' href='$provider' />\n";
+                echo "<link rel='openid.delegate' href='$delegate' />\n";
+                echo "<link rel='openid2.local_id' href='$provider' />\n";
+                echo "<link rel='openid2.provider' href='$delegate' />\n";
+            }
+        }
+    }
+}
+
+$openIdPlugin = new OpenIdDelegationPlugin;
+
+add_action('admin_init', array($openIdPlugin, 'init'));
+add_action('wp_head', array($openIdPlugin, 'renderMetaTags'));
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== OpenID Delegation ===
+Contributors: rodrigosprimo
+Tags: openid, identity, delegation
+Requires at least: 3.0
+Tested up to: 3.4.2
+Stable tag: 0.1
+
+A simple plugin that adds the required meta tags to allow you to use your blog URL as your OpenID with the provider you choose.
+
+== Description ==
+
+A simple plugin that adds the required meta tags to allow you to use your blog URL as your OpenID with the provider you choose.
+
+This plugins does not allow you to use your Wordpress installation as a OpenID consumer or provider.
+
+== Installation ==
+
+Use the standard WordPress plugin installation procedure. After installing and activating, go to
+the Settings > General to configure it. Once configured, the plugin will add meta tags to the header of your
+blog home page to delegate OpenID to your OpenID provider.
